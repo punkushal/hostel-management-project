50183170d30973ae84612e40506d10db6f041af2
punkushal <spycymagar@gmail.com>
2024-05-17 15:59:25 +0545

    Some modification made to the reusable text widget

--- lib/widgets/reusable_text.dart
+++ lib/widgets/reusable_text.dart
@@ -2,16 +2,28 @@ import 'package:flutter/material.dart';
 
 class ReusableText extends StatelessWidget {
   const ReusableText(
-      {super.key, required this.text, this.textAlign, this.textOverflow});
+      {super.key,
+      required this.text,
+      this.textAlign,
+      this.textOverflow,
+      this.fontWeight,
+      this.color,
+      this.fontSize,
+      required this.textStyle});
 
   final String text;
   final TextAlign? textAlign;
   final TextOverflow? textOverflow;
+  final FontWeight? fontWeight;
+  final Color? color;
+  final double? fontSize;
+  final TextStyle textStyle;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      style: textStyle,
     );
   }
 }
