9ba003c4c78c75f1939a160de98007a26bc2ef41
punkushal <spycymagar@gmail.com>
2024-05-19 16:36:48 +0545

    Loading screen added

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Loading...',
+          style: TextStyle(fontSize: 44),
+        ),
+      ),
+    );
+  }
+}
