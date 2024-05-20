fa3fb3a90746a99ad90707f34dbd45242180bf64
punkushal <spycymagar@gmail.com>
2024-05-20 09:47:29 +0545

    Modification made for fetching warden data

--- lib/screens/warden_home_screen.dart
+++ lib/screens/warden_home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:hostel_management_project/auth/controller/auth_controller.dart';
+import 'package:hostel_management_project/auth/controller/user_controller.dart';
+import 'package:hostel_management_project/auth/screens/login_screen.dart';
 import 'package:hostel_management_project/widgets/categories_list.dart';
 import 'package:hostel_management_project/widgets/custom_container.dart';
 
@@ -12,59 +16,92 @@ class WardenHomeScreen extends StatefulWidget {
 }
 
 class _WardenHomeScreenState extends State<WardenHomeScreen> {
+  AuthController authController = Get.put(AuthController());
+  final userController = Get.put(UserController());
   @override
   Widget build(BuildContext context) {
     Size mq = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: CustomContainer(
-              width: mq.width * 0.9,
-              height: mq.height * 0.2,
-              radiusValue: 12,
-              content: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Name",
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text("Hostel Name", style: TextStyle(fontSize: 18)),
-                      ],
-                    ),
-                    const SizedBox(
-                      width: 18,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        CupertinoIcons.calendar_badge_plus,
-                        size: 44,
-                      ),
-                    ),
-                  ],
-                ),
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                await authController.checkInternetConnection();
+                await authController.auth.signOut();
+                Get.offAll(() => const LoginScreen());
+              },
+              icon: const Icon(
+                Icons.logout,
+                size: 28,
               ),
             ),
-          ),
-          const Expanded(child: CategoriesList())
-        ],
-      ),
-    );
+            const SizedBox(
+              width: 8,
+            )
+          ],
+        ),
+        body: Obx(() => userController.isLoading.value
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue,
+                ),
+              )
+            : Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: CustomContainer(
+                      width: mq.width * 0.9,
+                      height: mq.height * 0.2,
+                      radiusValue: 12,
+                      content: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                userController.currentWarden != null
+                                    ? Text(
+                                        "Name : ${userController.currentWarden!.name}",
+                                        style: const TextStyle(fontSize: 18),
+                                      )
+                                    : const CircularProgressIndicator(
+                                        color: Colors.white,
+                                      ),
+                                const SizedBox(
+                                  height: 16,
+                                ),
+                                userController.currentWarden != null
+                                    ? Text(
+                                        "Hostel Name : ${userController.currentWarden!.hostelName}",
+                                        style: const TextStyle(fontSize: 18))
+                                    : const CircularProgressIndicator(
+                                        color: Colors.white,
+                                      )
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 18,
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                CupertinoIcons.calendar_badge_plus,
+                                size: 44,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Expanded(child: CategoriesList())
+                ],
+              )));
   }
 }
