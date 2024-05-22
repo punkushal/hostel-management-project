d65fe197bd75428860d6cdf5f9c8f8a522399aa6
punkushal <spycymagar@gmail.com>
2024-05-22 16:58:21 +0545

    Add student & student list design implemented

--- lib/screens/manage_students_screen.dart
+++ lib/screens/manage_students_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hostel_management_project/widgets/add_student_overlay.dart';
+
+import '../widgets/custom_container.dart';
 
 class ManageStudentsScreen extends StatelessWidget {
   const ManageStudentsScreen({super.key});
@@ -6,12 +9,68 @@ class ManageStudentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Manage Students'),
-          automaticallyImplyLeading: true,
-        ),
-        body: const Center(
-          child: Text('Manage students'),
-        ));
+      appBar: AppBar(
+        title: const Text('Manage Students'),
+        automaticallyImplyLeading: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => const AddStudentOverlay());
+              },
+              child: CustomContainer(
+                width: 200,
+                height: 200,
+                radiusValue: 12,
+                content: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/add-group.png',
+                      height: 60,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text('Add Student'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Center(
+            child: GestureDetector(
+              onTap: () {},
+              child: CustomContainer(
+                width: 200,
+                height: 200,
+                radiusValue: 12,
+                content: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/group.png',
+                      height: 60,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text('Students List'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
