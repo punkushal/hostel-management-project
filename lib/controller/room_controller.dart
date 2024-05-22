f089051f5b97e9b2d9939e678f7be23711ed0f56
punkushal <spycymagar@gmail.com>
2024-05-22 09:17:03 +0545

    Room Controller Added

---
+++ lib/controller/room_controller.dart
@@ -0,0 +1,40 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:hostel_management_project/auth/controller/auth_controller.dart';
+
+class RoomController extends GetxController {
+  AuthController authController = Get.put(AuthController());
+  RxList<String> roomNumbers = <String>[].obs;
+
+  Future<List<String>> fetchRoomNumbers(String wardenId) async {
+    try {
+      QuerySnapshot snapshot = await authController.database
+          .collection('rooms')
+          .where('wardenId', isEqualTo: wardenId)
+          .get();
+      List<String> roomNumbers = [];
+      for (var doc in snapshot.docs) {
+        int currentCapcity = int.parse(doc.get('currentCapacity'));
+        int capacity = int.parse(doc.get('capacity'));
+        if (currentCapcity <= capacity && currentCapcity != 0) {
+          roomNumbers.add(doc.get('roomNumber') as String);
+        }
+      }
+      return roomNumbers;
+    } catch (e) {
+      log('Error fetching room numbers : $e');
+      rethrow;
+    }
+  }
+
+  void fetchRoomData(String wardenId) async {
+    try {
+      List<String> rooms = await fetchRoomNumbers(wardenId);
+      roomNumbers.assignAll(rooms);
+    } catch (e) {
+      log('Error fetching room data : $e');
+    }
+  }
+}
