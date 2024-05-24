0fdcbd7afafefd6385547b8e651a5632cfc99965
punkushal <spycymagar@gmail.com>
2024-05-24 14:26:35 +0545

    Expense Income model added

---
+++ lib/models/expense_income.dart
@@ -0,0 +1,11 @@
+class ExpenseIncome {
+  final String title;
+  final String category;
+  final double amount;
+  final DateTime dateTime;
+  ExpenseIncome(
+      {required this.title,
+      required this.amount,
+      required this.dateTime,
+      required this.category});
+}
