4c11970447330d8ae398c15ca037ac2c6ef5daab
punkushal <spycymagar@gmail.com>
2024-05-26 19:28:38 +0545

    Confirm Password Toggle Visibility Functionality Added

--- lib/auth/controller/password_controller.dart
+++ lib/auth/controller/password_controller.dart
@@ -2,8 +2,13 @@ import 'package:get/get.dart';
 
 class PasswordController extends GetxController {
   var isPasswordVisible = false.obs;
+  var isConfirmPasswordVisible = false.obs;
 
   void togglePasswordVisibility() {
+    isPasswordVisible.value = !isPasswordVisible.value;
+  }
+
+  void toggleConfirmPasswordVisibility() {
     isPasswordVisible.value = !isPasswordVisible.value;
   }
 }
